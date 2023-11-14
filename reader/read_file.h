745d8210613d09f0d2084484bfbb7e6a324894a0
tienloi244 <tienloi2125@gmail.com>
2023-11-14 16:11:22 +0700

    Add structure for reading files.

---
+++ reader/read_file.h
@@ -0,0 +1,21 @@
+#ifndef __READ_FILE__
+#define __READ_FILE__
+#include <iostream>
+#include <unordered_map>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include "../libraries/rapidxml.hpp"
+
+using namespace rapidxml;
+using namespace std;
+
+struct Entity
+{
+    string entity_type = "";
+    unordered_map<string, string> attributes;
+    string content = "";
+};
+
+#endif
